b6a38eef74e1a7b219a00c041be0ef842c5eaad9
ClothAtTheFair <bradleybsf12@gmail.com>
2019-12-23 22:07:18 -0500

    Init

---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Got Questions</h2>
+                <p>me too</p>
+            </div>
+        );
+    }
+}
+
+export default Contact;
